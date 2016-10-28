fe0889512cf2510289ade03a065146baf9639f57
Mitch Bitzan <mitchell.bitzan@gmail.com>
2016-10-28 18:48:18 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -33,14 +33,14 @@ CREATE TABLE Subscription (
   sid varchar2(255),
   slen varchar2(50) default NULL,
   begin_date varchar2(255),
-  fee varchar2(100) default NULL
+  fee varchar2(100) default NULL,
   PRIMARY KEY (sid)
 );
 
-CREATE TABLE User (
-  uid varchar2(20) default NULL,
+CREATE TABLE Users (
+  user_id varchar2(20) default NULL,
   uname varchar2(255) default NULL,
-  PRIMARY KEY (uid)
+  PRIMARY KEY (user_id)
 );
 
 CREATE TABLE Viewer (
@@ -54,14 +54,103 @@ CREATE TABLE Catalog_item (
   PRIMARY KEY (catid)
 );
 
--- Journal
-INSERT INTO Journal (jid,jtitle,jvol,content)
+CREATE TABLE Journal (
+  jid varchar2(255),
+  jtitle varchar2(255),
+  jvol number,
+  content varchar2(255),
+  catid varchar2(255) NOT NULL references Catalog_item(catid),
+  PRIMARY KEY (jid, catid) );
 
--- Magazine
-INSERT INTO Magazine (mid,mtitle,mvol,content)
+CREATE TABLE Magazine (
+  mid varchar2(255),
+  mtitle varchar2(255),
+  jvol number,
+  content varchar2(255),
+  catid varchar2(255) NOT NULL references Catalog_item(catid),
+  PRIMARY KEY (mid, catid) ); 
+  
+CREATE TABLE Book (
+  bid varchar2(255),
+  btitle varchar2(255),
+  jvol number,
+  content varchar2(255),
+  catid varchar2(255) NOT NULL references Catalog_item(catid),
+  PRIMARY KEY (bid, catid) );
 
--- Book
-INSERT INTO Book (bid,btitle,edition,content)
+CREATE TABLE Conference_proceedings (
+  cid varchar2(255),
+  cptitle varchar2(255),
+  content varchar2(255),
+  catid varchar2(255) NOT NULL references Catalog_item(catid),
+  PRIMARY KEY (cid, catid) );
 
--- Journal
-INSERT INTO Conference_proceedings (cpid,cptitle,content)
+
+CREATE TABLE Reference (
+  catid varchar2(255),
+  cite_id varchar2(255),
+  PRIMARY KEY (catid, cite_id),
+  FOREIGN KEY (catid) references Catalog_item );
+
+CREATE TABLE creates (
+  vid  number,  
+  sid  varchar2(255) NOT NULL, 
+  user_id  varchar2(255) NOT NULL,
+  PRIMARY KEY (vid, sid, user_id),
+  FOREIGN KEY (vid) references Viewer,
+  FOREIGN KEY (sid) references Subscription,
+  FOREIGN KEY (user_id) references Users );  
+
+CREATE TABLE Search (
+  vid number NOT NULL, 
+  catid varchar2(255),
+  PRIMARY KEY (vid, catid),
+  FOREIGN KEY (vid) references Viewer,
+  FOREIGN KEY (catid) references Catalog_item );
+  
+CREATE TABLE Retrieve (
+  user_id varchar2(255),
+  catid varchar2(255),
+  PRIMARY KEY (user_id, catid),
+  FOREIGN KEY (user_id) references Users,
+  FOREIGN KEY (catid) references Catalog_item ); 
+  
+CREATE TABLE Writes (
+  catid varchar2(255) NOT NULL,
+  pid number NOT NULL,
+  PRIMARY KEY (catid, pid),
+  FOREIGN KEY (catid) references Catalog_item, 
+  FOREIGN KEY (pid) references Author );  
+  
+CREATE TABLE Edits (
+  catid varchar2(255) NOT NULL,
+  pid number NOT NULL,
+  PRIMARY KEY (catid, pid),
+  FOREIGN KEY (catid) references Catalog_item, 
+  FOREIGN KEY (pid) references Editor );   
+  
+CREATE TABLE Publishes (
+  catid varchar2(255) NOT NULL,
+  pcid number NOT NULL, 
+  year number,
+  PRIMARY KEY (catid, pcid),
+  FOREIGN KEY (catid) references Catalog_item,
+  FOREIGN KEY (pcid) references Publishing_Company );
+
+CREATE TABLE Located_at (
+  pcid number NOT NULL,
+  street varchar2(255),
+  city varchar(255),
+  state varchar(255),
+  PRIMARY KEY (pcid, street, city, state),
+  FOREIGN KEY (pcid) references Publishing_Company,
+  FOREIGN KEY (street, city, state) references Address );
+
+CREATE TABLE Lives_At (
+  pid number NOT NULL, 
+  street varchar2(255),
+  city varchar2(255),
+  state varchar2(255),
+  PRIMARY KEY (pid, street, city, state),
+  FOREIGN KEY (pid) references Person,
+  FOREIGN KEY (street, city, state) references Address );
