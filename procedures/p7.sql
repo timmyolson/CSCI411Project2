9c583e2a9a203926093b3ff91aa6c614b170fb71
timmyolson <olti1201@stcloudstate.edu>
2016-11-02 15:16:36 -0500

    Update p7.sql

--- procedures/p7.sql
+++ procedures/p7.sql
@@ -22,3 +22,23 @@ SELECT C.catid, C.contract_term
 FROM Catalog_item C
 WHERE C.contract_term < (SELECT SYSDATE
                          FROM DUAL);
+
+-- SQL> @p7
+-- 
+-- CATID      CONTRACT_TERM
+-------- -------------
+-- NJEQ6977   25-JAN-05
+-- ABIQ2755   03-JUL-12
+-- VMGS7843   22-MAY-11
+-- ZNMF0012   02-NOV-16
+-- 
+-- 
+-- Procedure created.
+-- 
+-- 
+-- PL/SQL procedure successfully completed.
+-- 
+-- 
+-- no rows selected
+-- 
+-- SQL>
